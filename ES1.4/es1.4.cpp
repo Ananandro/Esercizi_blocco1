34aa0b7563db3622ce54d5fbee501603fb65f23f
Ananandro <andryape555@gmail.com>
2020-11-16 13:15:42 +0100

    Programma che spamma multipli di 2 (loop infinito)

---
+++ ES1.4/es1.4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+
+    int i = 0;
+    int n = 2;
+
+    cout << "Madonna ora ti spammo tanti di quei multipli di 2 che tu non immagini neanche" << endl;
+
+    while (i == 0){
+
+        cout << n << endl;
+        n +=2;
+        
+    }
+
+    return 0;
+
+}
